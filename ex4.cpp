e7af0ae1bc85d18ace88d8e3779054ea0ba6f252
FabiaIsabella <fabia.isape@gmail.com>
2018-08-10 08:35:50 -0300

    ex4 e exemplo ex3

--- ex4.cpp
+++ ex4.cpp
@@ -1,45 +1,62 @@
-//g++ ex2.cpp -o ex2 `pkg-config --cflags --libs opencv`
+//g++ ex4.cpp -o ex4 `pkg-config --cflags --libs opencv`
 
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
 #include <iostream>
+#include "dirent.h"
 
 using namespace std;
 using namespace cv;
  
 int main() {
   
-	Mat img;
+	string input = "input";
+	string output = "output";
 	int height, width;
-
-	img = imread("lena.jpg" , CV_LOAD_IMAGE_COLOR);  
-
-	if(!img.data) {
-		cout <<  "Não foi possivel abrir a imagem" << endl ;
+	Mat img;
+	
+	DIR *directory = opendir (input.c_str());
+	struct dirent *_dirent = NULL;
+	
+	if(directory == NULL){
+		cout << "Erro ao abrir a pasta" << endl;
 		return -1;
 	}
+
+	while((_dirent = readdir(directory)) != NULL){
+        	string fileName = input + "/" + string(_dirent->d_name);
+       		
+		img = imread(fileName.c_str());
+        	
+		if(!img.data){
+           		cout << "Erro ao abrir imagem" << endl;
+			continue;
+        	}
+		
+		imshow(fileName, img);
+
+		width = img.cols;
+		height = img.rows;
 	
-	imshow("Imagem 1", img);
-  
-	width = img.cols;
-	height = img.rows;
-	
-	for(int i = 0; i<width; i++){
-		for(int j = 0; j<height; j++){
+		for(int i = 0; i<width; i++){
+			for(int j = 0; j<height; j++){
 			
-			// RGB
-			// se r+100 > 255 entao r = 255 senao r = r+100
-			img.at<Vec3b>(i,j)[0] = ((img.at<Vec3b>(i,j)[0]+100) > 255) ? 255 : img.at<Vec3b>(i,j)[0]+100;
-			img.at<Vec3b>(i,j)[1] = ((img.at<Vec3b>(i,j)[1]+100) > 255) ? 255 : img.at<Vec3b>(i,j)[1]+100;
-			img.at<Vec3b>(i,j)[2] = ((img.at<Vec3b>(i,j)[2]+100) > 255) ? 255 : img.at<Vec3b>(i,j)[2]+100;
-		}	
-	}
+				// RGB
+				// se r+100 > 255 entao r = 255 senao r = r+100
+				img.at<Vec3b>(i,j)[0] = ((img.at<Vec3b>(i,j)[0]+100) > 255) ? 255 : img.at<Vec3b>(i,j)[0]+100;
+				img.at<Vec3b>(i,j)[1] = ((img.at<Vec3b>(i,j)[1]+100) > 255) ? 255 : img.at<Vec3b>(i,j)[1]+100;
+				img.at<Vec3b>(i,j)[2] = ((img.at<Vec3b>(i,j)[2]+100) > 255) ? 255 : img.at<Vec3b>(i,j)[2]+100;
+			}	
+		}
 
-	imwrite("ex2.jpg",img);
 
-	imshow("Imagem 2", img);
-	  
+        	fileName = output + "/" + string(_dirent->d_name);
+        	imwrite(fileName.c_str(), img);
+    	}
+    	
+	closedir(directory);	
 	waitKey(0);
+	
 	return 0;
 }
--- python/ex3.py
+++ python/ex3.py
@@ -1,16 +1,33 @@
-import numpy as np
 import cv2
-
-cap = cv2.VideoCapture('vtest.avi')
-
+import numpy as np
+ 
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture(0)
+ 
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
+ 
+# Read until video is completed
 while(cap.isOpened()):
-    ret, frame = cap.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  if ret == True:
+ 
+    # Display the resulting frame
+    cv2.imshow('Frame',frame)
+ 
+    # Press Q on keyboard to  exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
+ 
+  # Break the loop
+  else: 
+    break
+ 
+# When everything done, release the video capture object
 cap.release()
+ 
+# Closes all the frames
 cv2.destroyAllWindows()
