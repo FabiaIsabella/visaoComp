6e54a6de1caa0c848b8088ce895e6aee32800e18
FabiaIsabella <fabia.isape@gmail.com>
2018-08-10 07:27:18 -0300

    ex1 e ex2 opencv

---
+++ ex1.cpp
@@ -0,0 +1,48 @@
+//g++ ex1.cpp -o ex1 `pkg-config --cflags --libs opencv`
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+ 
+int main() {
+  
+	Mat img;
+	int height, width, pixel;
+
+	img = imread("lena.jpg" , CV_LOAD_IMAGE_GRAYSCALE);  
+
+	if(!img.data) {
+		cout <<  "Não foi possivel abrir a imagem" << endl ;
+		return -1;
+	}
+	
+	imshow("Imagem 1", img);
+  
+	width = img.cols;
+	height = img.rows;
+	
+	for(int i = 0; i<width; i++){
+		for(int j = 0; j<height; j++){
+			pixel = (int)img.at<uchar>(i,j);
+			
+			pixel = pixel + 100;
+			
+			if(pixel > 255) pixel = 255;
+			
+			cout << pixel << endl;		
+			
+			img.at<uchar>(i,j) = pixel;
+		}	
+	}
+	
+        imwrite("ex1.jpg",img);
+
+	imshow("Imagem 2", img);
+	  
+	waitKey(0);
+	return 0;
+}
---
+++ ex2.cpp
@@ -0,0 +1,45 @@
+//g++ ex2.cpp -o ex2 `pkg-config --cflags --libs opencv`
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+ 
+int main() {
+  
+	Mat img;
+	int height, width;
+
+	img = imread("lena.jpg" , CV_LOAD_IMAGE_COLOR);  
+
+	if(!img.data) {
+		cout <<  "Não foi possivel abrir a imagem" << endl ;
+		return -1;
+	}
+	
+	imshow("Imagem 1", img);
+  
+	width = img.cols;
+	height = img.rows;
+	
+	for(int i = 0; i<width; i++){
+		for(int j = 0; j<height; j++){
+			
+			// RGB
+			// se r+100 > 255 entao r = 255 senao r = r+100
+			img.at<Vec3b>(i,j)[0] = ((img.at<Vec3b>(i,j)[0]+100) > 255) ? 255 : img.at<Vec3b>(i,j)[0]+100;
+			img.at<Vec3b>(i,j)[1] = ((img.at<Vec3b>(i,j)[1]+100) > 255) ? 255 : img.at<Vec3b>(i,j)[1]+100;
+			img.at<Vec3b>(i,j)[2] = ((img.at<Vec3b>(i,j)[2]+100) > 255) ? 255 : img.at<Vec3b>(i,j)[2]+100;
+		}	
+	}
+
+	imwrite("ex2.jpg",img);
+
+	imshow("Imagem 2", img);
+	  
+	waitKey(0);
+	return 0;
+}
